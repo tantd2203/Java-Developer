66e99620f6b6e2d19e5db870f2406c204db1882a
hung <your_email@example.com>
2024-07-10 12:10:18 +0700

    validateEnum

---
+++ src/main/java/com/programming/uit/javadeveloper/dto/Address.java
@@ -0,0 +1,19 @@
+package com.programming.uit.javadeveloper.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public  class Address {
+
+    private String apartmentNumber;
+    private String floor;
+    private String building;
+    private String streetNumber;
+    private String street;
+    private String city;
+    private String country;
+    private Integer addressType;
+}
